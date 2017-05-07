496280dd5c6e767b882d00b4149ece147dad73ab
邓翔宇 <dengxiangyu@dengxiangyudeMacBook-Pro.local>
2017-05-07 21:53:38 +0800

    update

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,PasswordField,BooleanField
 from wtforms.validators import Required
 
 class NameForm(FlaskForm):
     name = StringField('what is your name?',validators=[Required()])
     submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    user = StringField(validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log in')
--- migrations/versions/93f9d560816f_init_db.py
+++ migrations/versions/93f9d560816f_init_db.py
@@ -1,8 +1,8 @@
-"""init db
+"""init_db
 
-Revision ID: 8b8049e566d0
+Revision ID: 93f9d560816f
 Revises: 
-Create Date: 2017-05-06 16:40:45.859915
+Create Date: 2017-05-07 21:16:46.227738
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8b8049e566d0'
+revision = '93f9d560816f'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -28,6 +28,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=64), nullable=True),
     sa.Column('role_id', sa.Integer(), nullable=True),
+    sa.Column('password_hash', sa.String(length=128), nullable=True),
     sa.ForeignKeyConstraint(['role_id'], ['roles.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
